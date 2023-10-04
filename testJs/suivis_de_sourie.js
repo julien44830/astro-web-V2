ca2bed7f987649f9c89ec0c23868d4fe2c55018c
Julien DENIAUD <deniaudjulien0@gmail.com>
2023-10-04 20:48:54 +0200

    go

--- testJs/suivis_de_sourie.js
+++ testJs/suivis_de_sourie.js
@@ -1,17 +1,11 @@
-
-
 const ombre = document.createElement("div");
 
 document.body.appendChild(ombre);
 
-
 document.body.addEventListener("mousemove", e => {
-    console.log(e);
-    console.log('hello world');
-    console.log("pageX: ", e.pageX, 
-    "pageY: ", e.pageY, 
-    "clientX: ", e.clientX, 
-    "clientY:", e.clientY);
+    
+
+    console.log("pageX: ", e.pageX, "pageY: ", e.pageY, "clientX: ", e.clientX, "clientY:", e.clientY);
 
     ombre.classList = "ombre";
     ombre.style.left = e.clientX + "px",
