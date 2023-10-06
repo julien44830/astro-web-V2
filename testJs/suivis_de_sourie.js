ee938b852d24f1e7ae785894a92d0935670bd27a
julien44830 <deniaudjulien0@gmail.com>
2023-10-07 01:52:58 +0200

    suppression div mouseout

--- testJs/suivis_de_sourie.js
+++ testJs/suivis_de_sourie.js
@@ -1,13 +1,25 @@
 const ombre = document.createElement("div");
 
-document.body.appendChild(ombre);
+
+document.addEventListener("mouseout", e => {
+
+    console.log('je suis sortie');
+    ombre.classList.add('ombre2');
+
+    setTimeout(() => {
+        ombre.classList.remove('ombre');
+    }, 500);
+});
 
 document.body.addEventListener("mousemove", e => {
-    
+    document.body.appendChild(ombre);
 
-    console.log("pageX: ", e.pageX, "pageY: ", e.pageY, "clientX: ", e.clientX, "clientY:", e.clientY);
 
+    ombre.classList.remove('ombre2');
     ombre.classList = "ombre";
-    ombre.style.left = e.clientX + "px",
-    ombre.style.top = e.clientY + "px";
+    ombre.style.display ="block";
+    ombre.style.left = e.clientX + -18 + "px",
+    ombre.style.top = e.clientY + -18 + "px";
+
+    e.stopPropagation();
 });
