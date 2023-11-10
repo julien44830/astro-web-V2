c986e89dfcfe8fc00677287f9505ebd4ffe2b6c9
Julien DENIAUD <deniaudjulien0@gmail.com>
2023-11-10 08:19:08 +0100

    bug transition

--- js/suivis_de_sourie.js
+++ js/suivis_de_sourie.js
@@ -14,31 +14,54 @@ img.style.display = "none";
 document.body.appendChild(ombre);
 ombre.appendChild(img);
 
+// window.addEventListener("mousemove", e => {
+//     e.stopPropagation();
+//     ombre.style.pointerEvents = "none";
+
+//     const x = e.clientX;
+//     const y = window.innerHeight - e.clientY; // Inversez la coordonnée Y
+
+//     const windowWidth = window.innerWidth;
+//     const windowHeight = window.innerHeight;
+
+//     // Calculez la position normalisée le long de la diagonale inversée
+//     const normalizedDiagonalPosition = (x + y) / (windowWidth + windowHeight);
+
+//     // La valeur sera entre 0 (coin inférieur gauche) et 1 (coin supérieur droit)
+//     console.log("Position normalisée : " + normalizedDiagonalPosition);    
+//         // Utilisez la valeur normalisée comme vous le souhaitez
+
+//     img.style.display = "block";
+//     // console.log("x = " + e.clientX + "  y = " + e.clientY);
+
+//         img.classList.remove('ombre2');
+//         img.classList.add("ombre");
+
+//         img.style.left = e.clientX - 18 + "px";
+//         img.style.top = e.clientY - 18 + "px";
+//         img.style.height = normalizedDiagonalPosition *45 + "px";
+//         img.style.opacity = normalizedDiagonalPosition -0.3;
+// });
+
+
 window.addEventListener("mousemove", e => {
     e.stopPropagation();
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
     ombre.style.pointerEvents = "none";
 
     const x = e.clientX;
     const y = window.innerHeight - e.clientY; // Inversez la coordonnée Y
-
-    const windowWidth = window.innerWidth;
-    const windowHeight = window.innerHeight;
+    console.log(x,y);
 
     // Calculez la position normalisée le long de la diagonale inversée
     const normalizedDiagonalPosition = (x + y) / (windowWidth + windowHeight);
 
-    // La valeur sera entre 0 (coin inférieur gauche) et 1 (coin supérieur droit)
-    console.log("Position normalisée : " + normalizedDiagonalPosition);    
-        // Utilisez la valeur normalisée comme vous le souhaitez
-
+    img.classList.remove('ombre2');
+    img.classList.add("ombre");
     img.style.display = "block";
-    // console.log("x = " + e.clientX + "  y = " + e.clientY);
-
-        img.classList.remove('ombre2');
-        img.classList.add("ombre");
-
-        img.style.left = e.clientX - 18 + "px";
-        img.style.top = e.clientY - 18 + "px";
-        img.style.height = normalizedDiagonalPosition *45 + "px";
-        img.style.opacity = normalizedDiagonalPosition -0.3;
+    img.style.left = x - 18 + "px";
+    img.style.top = e.clientY - 18 + "px";
+    img.style.height = normalizedDiagonalPosition * 45 + "px";
+    img.style.opacity = normalizedDiagonalPosition - 0.3;
 });
--- js/transition.js
+++ js/transition.js
@@ -1,5 +1,21 @@
+const dark = "#02023d";
+const light = "#fff";
+
 const btn_page = document.querySelectorAll("a");
 const img_transition = document.querySelector(".transition");
+
+window.onload 
+  if (switchTheme.checked) {
+    setTheme(true);
+    img_transition.style.backgroundColor = light;
+  } else {
+    setTheme(false);
+    img_transition.style.backgroundColor = dark;
+  }
+
+
+
+
 
 for (const btn of btn_page) {
   btn.addEventListener('click', function (event) {
@@ -8,6 +24,14 @@ for (const btn of btn_page) {
 
     // Supprimez temporairement l'attribut href pour désactiver le lien
     btn.removeAttribute('href');
+
+    if (switchTheme.checked) {
+      setTheme(true);
+      img_transition.style.backgroundColor = light; // Remplacez 'votre_couleur' par la couleur souhaitée
+    } else {
+      setTheme(false);
+      img_transition.style.backgroundColor = dark; // Remplacez 'votre_couleur' par la couleur souhaitée
+    }
 
     // Déclenchez l'animation de transition de l'image
     img_transition.style.animation = 'transition2 1.5s ease-in-out forwards';
@@ -22,3 +46,39 @@ for (const btn of btn_page) {
     });
   });
 }
+
+
+
+
+// for (const btn of btn_page) {
+//   btn.addEventListener('click', function (event) {
+//     event.preventDefault(); // Empêche le lien de rediriger immédiatement
+//     const destinationURL = btn.getAttribute('href');
+
+//     // Supprimez temporairement l'attribut href pour désactiver le lien
+//     btn.removeAttribute('href');
+
+//     // Définissez la couleur de l'arrière-plan
+//     if (switchTheme.checked) {
+//       setTheme(true);
+//       img_transition.style.backgroundColor = light; // Remplacez 'votre_couleur' par la couleur souhaitée
+//     } else {
+//       setTheme(false);
+//       img_transition.style.backgroundColor = dark; // Remplacez 'votre_couleur' par la couleur souhaitée
+//     }
+
+//     // Déclenchez l'animation de transition de l'image
+//     img_transition.style.animation = 'transition2 1.5s ease-in-out forwards';
+
+//     // Attendez la fin de l'animation pour rétablir l'attribut href et activer le lien
+//     img_transition.addEventListener('animationend', function() {
+//       // Rétablissez l'attribut href pour réactiver le lien
+//       btn.setAttribute('href', destinationURL);
+
+//       // Réinitialisez la couleur de l'arrière-plan
+//       img_transition.style.backgroundColor = ''; // Réinitialisez la couleur
+//       // Activez la redirection vers la nouvelle page
+//       window.location.href = destinationURL;
+//     });
+//   });
+// }
