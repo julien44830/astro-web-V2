8a4306551f6c97ed6c3953e5eb5c7a3a4a1481f3
deniaud julien <deniaudjulien0@gmail.com>
2024-01-15 07:24:23 +0100

    theme storage & hour

--- js/meteo.js
+++ js/meteo.js
@@ -1,35 +1,3 @@
-// let div = document.getElementById('idc100a5bd966');
-// let new_div;
-// let country ;
-
-// function getCityFromIP() {
-//     // Faites une requête à l'API ipinfo.io pour obtenir les informations de l'adresse IP
-//     fetch('https://ipinfo.io/json')
-//       .then(response => response.json())
-//       .then(data => {
-//         // Extrayez la ville à partir des données
-//         country = data.city;
-        
-//         // Utilisez la variable city comme vous le souhaitez
-//         console.log('Ville basée sur l\'adresse IP :', country);
-//       })
-//       .catch(error => {
-//         console.error('Erreur lors de la récupération de la ville :', error);
-//       });
-//   }
-  
-//   // Appelez la fonction pour obtenir la ville basée sur l'adresse IP
-//   getCityFromIP();
-  
-
-// let href = `<a href=\"https://meteolabs.fr/meteo_${country}/demain/\">prévisions pour demain à Nantes</a>`;
-
-
-// new_div = div.innerHTML += href;
-
-// console.log(new_div);
-
-
 let div = document.getElementById('meteo');
 let country;
 
@@ -82,8 +50,4 @@ function getCityFromIP() {
 // Appelez la fonction pour obtenir la ville basée sur l'adresse IP
 getCityFromIP();
 
-
-
-// <div id="idc100a5bd966" a='{"t":"v","v":"1.2","lang":"fr","locs":[7],"ssot":"c","sics":"ds","cbkg":"#9E9E9E","cfnt":"#FFFFFF","cprb":"#616161","cprf":"#FFFFFF","slmw":226,"slbr":4,"slfs":20,"sfnt":"a"}'>Source des données météo: <a href="https://meteolabs.fr/meteo_${country}/demain/">météo pour demain ${country}</a></div><script async src="https://static1.meteolabs.fr/widgetjs/?id=idc100a5bd966"></script>
-
-a="{&quot;t&quot;:&quot;v&quot;,&quot;v&quot;:&quot;1.2&quot;,&quot;lang&quot;:&quot;fr&quot;,&quot;locs&quot;:[7],&quot;ssot&quot;:&quot;c&quot;,&quot;sics&quot;:&quot;ds&quot;,&quot;cbkg&quot;:&quot;#9E9E9E&quot;,&quot;cfnt&quot;:&quot;#FFFFFF&quot;,&quot;cprb&quot;:&quot;#616161&quot;,&quot;cprf&quot;:&quot;#FFFFFF&quot;,&quot;slmw&quot;:226,&quot;slbr&quot;:4,&quot;slfs&quot;:20,&quot;sfnt&quot;:&quot;a&quot;}"
+// a="{&quot;t&quot;:&quot;v&quot;,&quot;v&quot;:&quot;1.2&quot;,&quot;lang&quot;:&quot;fr&quot;,&quot;locs&quot;:[7],&quot;ssot&quot;:&quot;c&quot;,&quot;sics&quot;:&quot;ds&quot;,&quot;cbkg&quot;:&quot;#9E9E9E&quot;,&quot;cfnt&quot;:&quot;#FFFFFF&quot;,&quot;cprb&quot;:&quot;#616161&quot;,&quot;cprf&quot;:&quot;#FFFFFF&quot;,&quot;slmw&quot;:226,&quot;slbr&quot;:4,&quot;slfs&quot;:20,&quot;sfnt&quot;:&quot;a&quot;}"
--- js/switch.js
+++ js/switch.js
@@ -17,81 +17,45 @@ function setTheme(isLightTheme) {
 }
 
 // Fonction pour basculer le thème et enregistrer l'état dans le localStorage
-function toggleTheme() {
-  if (switchTheme.checked) {
-    setTheme(true);
-    localStorage.setItem('theme', 'light'); // Enregistrez le thème dans le localStorage
-  } else {
-    setTheme(false);
-    localStorage.setItem('theme', 'dark'); // Enregistrez le thème dans le localStorage
-  }
+function toggleTheme(isLightTheme) {
+  setTheme(isLightTheme);
+  localStorage.setItem('theme', isLightTheme ? 'light' : 'dark'); // Enregistrez le thème dans le localStorage
 }
 
-// Ajoutez un gestionnaire d'événement pour le changement de thème
-switchTheme.addEventListener('change', toggleTheme);
+// Fonction pour déterminer si le thème doit être clair ou foncé en fonction de l'heure
+function isLightThemeNow() {
+  const currentHour = new Date().getHours();
+  return currentHour >= 8 && currentHour < 19;
+}
 
 // Récupérez le thème enregistré dans le localStorage
 const savedTheme = localStorage.getItem('theme');
+
 if (savedTheme) {
-  if (savedTheme === 'light') {
-    switchTheme.checked = true;
-    setTheme(true);
-  } else {
-    switchTheme.checked = false;
-    setTheme(false);
-  }
+  // Si le thème est enregistré en localStorage, utilisez-le
+  switchTheme.checked = savedTheme === 'light';
+  setTheme(savedTheme === 'light');
+} else {
+  // Si aucun thème n'est enregistré en localStorage, utilisez le thème en fonction de l'heure
+  const isLightNow = isLightThemeNow();
+  switchTheme.checked = isLightNow;
+  setTheme(isLightNow);
 }
 
+// Ajoutez un gestionnaire d'événement pour le changement de thème
+switchTheme.addEventListener('change', function () {
+  toggleTheme(switchTheme.checked);
+});
 
-/*******************code qui prend en charge l'heure pour changer le thème****************/
+// Mise à jour automatique du thème en fonction de l'heure
+setInterval(function () {
+  if (!savedTheme) {
+    const isLightNow = isLightThemeNow();
+    if (switchTheme.checked !== isLightNow) {
+      switchTheme.checked = isLightNow;
+      toggleTheme(isLightNow);
+    }
+  }
+}, 1000 * 60 * 60); // Met à jour toutes les heures (3600000 millisecondes)
 
-// const switchTheme = document.querySelector("#hide-checkbox");
-// const body2Elements = document.getElementsByClassName("pointerEvent");
-
-// // Fonction pour changer le thème
-// function setTheme(isLightTheme) {
-//   if (isLightTheme) {
-//     for (const element of body2Elements) {
-//       element.classList.remove('body2');
-//     }
-//   } else {
-//     for (const element of body2Elements) {
-//       element.classList.add('body2');
-//     }
-//   }
-// }
-
-// // Fonction pour basculer le thème et enregistrer l'état dans le localStorage
-// function toggleTheme(isLightTheme) {
-//   setTheme(isLightTheme);
-//   localStorage.setItem('theme', isLightTheme ? 'light' : 'dark'); // Enregistrez le thème dans le localStorage
-// }
-
-// // Fonction pour déterminer si le thème doit être clair ou foncé en fonction de l'heure
-// function isLightThemeNow() {
-//   const currentHour = new Date().getHours();
-//   return currentHour >= 10 && currentHour < 19;
-// }
-
-// // Initialise le thème en fonction de l'heure actuelle
-// const initialTheme = isLightThemeNow();
-// toggleTheme(initialTheme);
-// switchTheme.checked = initialTheme;
-
-// // Ajoute un gestionnaire d'événement pour le changement de thème
-// switchTheme.addEventListener('change', function () {
-//   toggleTheme(switchTheme.checked);
-// });
-
-// // Mise à jour automatique du thème en fonction de l'heure
-// setInterval(function () {
-//   const isLightNow = isLightThemeNow();
-//   if (switchTheme.checked !== isLightNow) {
-//     switchTheme.checked = isLightNow;
-//     toggleTheme(isLightNow);
-//   }
-// }, 1000 * 60 * 60); // Met à jour toutes les heures (3600000 millisecondes)
-
-
-/**************************nouveau code********************************/
 
